bfeeaba4bf8e6f86ac709439f9b620909ebe913a
Abigail Schidler <abigailschidler15@gmail.com>
2025-06-18 20:17:55 -0700

    finished commenting

--- june-2025/june-18/attempted/contains_duplicate.py
+++ june-2025/june-18/attempted/contains_duplicate.py
@@ -5,8 +5,10 @@ from typing import List
 class Solution:
 
   def hasDuplicate(self, nums: List[int]) -> bool:
+    # set of characters found in nums array
     found = set()
     for num in nums:
+      # case when found already contains the character
       if num in found:
         return True
       else:
